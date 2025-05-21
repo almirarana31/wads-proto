cd5f506654dfbf095c2582f84b12d3bfeb6c20c8
Ariel PD <arielprandi34315@gmail.com>
2025-05-21 23:56:42 +0700

    Added relations to tables
    Signed-off-by: Ariel PD <arielprandi34315@gmail.com>

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,6 @@
 import Customer from "./Customer.js";
 import Staff from './Staff.js';
+import User from './User.js';
 import StaffDetail from './StaffDetail.js';
 import Role from './enum/Role.js';
 import Job from './enum/Job.js';
@@ -9,8 +10,31 @@ import Status from "./enum/Status.js";
 import Priority from "./enum/Priority.js";
 import Ticket from "./Ticket.js";
 
-// Define references here
+// StaffDetail
+StaffDetail.belongsTo(Role, {foreignKey: 'role_id'})
+StaffDetail.belongsTo(Field, {foreignKey: 'field_id'})
+StaffDetail.belongsTo(Job, {foreignKey: 'job_id'})
 
+// Staff
+Staff.belongsTo(StaffDetail, {foreignKey: 'detail_id'})
+
+// User
+User.hasOne(Staff, {foreignKey: 'id'})
+User.hasOne(User, {foreignKey: 'id'})
+
+// Ticket 
+Ticket.belongsTo(User, {foreignKey: 'user_id'})
+Ticket.belongsTo(Staff, {foreignKey: 'id'})
+Ticket.belongsTo(Category, {foreignKey: 'id'})
+Ticket.belongsTo(Priority, {foreignKey: 'id'})
+Ticket.belongsTo(Status, {foreignKey: 'id'})
+
+// Ticket model: userID and staffID both refer to User table
+Ticket.belongsTo(Category, {foreignKey: 'category'});
+Ticket.belongsTo(Status, {foreignKey: 'status'});
+Ticket.belongsTo(Priority, {foreignKey: 'priority'});
+Ticket.belongsTo(User, { foreignKey: 'userID', as: 'User' });
+Ticket.belongsTo(User, { foreignKey: 'staffID', as: 'Staff' });
 
 export {
     Customer,
