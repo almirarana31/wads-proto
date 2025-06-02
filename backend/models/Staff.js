ae96f23f83c21f0a358fd96c9716fd489de3cc72
almirarana31 <144789177+almirarana31@users.noreply.github.com>
2025-06-02 16:18:13 +0700

    staff model update from ariel

--- backend/models/Staff.js
+++ backend/models/Staff.js
@@ -21,7 +21,7 @@ Staff.init(
        },
        field_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: 'category',
             key: 'id'
