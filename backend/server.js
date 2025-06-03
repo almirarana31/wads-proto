8be258362018a357d5c0bd01794eb343cdb354de
orlando <144786542+orlandojpwastaken@users.noreply.github.com>
2025-06-03 07:34:14 +0700

    why do we have server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ import fs from 'fs';
 import users_router from './routes/user_routes.js';
 const app = express();
 
+const corsOptions = {
+  origin: 'https://e2425-wads-l4ccg3-client.csbihub.id', // your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true, // Allow cookies, authorization headers
+};
+
+// Use CORS middleware BEFORE your routes
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api', users_router);
@@ -16,7 +24,7 @@ app.use('/api', users_router);
 // })
 
 // PORT, .env variables
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 30;
 
 app.listen(port, (req, res) => {
     console.log(`Listening on port ${port}`);
