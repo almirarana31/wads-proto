1f159c7ada1d5534e2686195ef32c8667a3f78a9
almirarana31 <144789177+almirarana31@users.noreply.github.com>
2025-06-09 17:31:26 +0700

    documentation

---
+++ docs-site/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\almir\\wads-proto-actual\\wads-proto\\docs-site\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\almir\\wads-proto-actual\\wads-proto\\docs-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\almir\\wads-proto-actual\\wads-proto\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\almir\\wads-proto-actual\\wads-proto\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\almir\\wads-proto-actual\\wads-proto\\docs-site\\src\\css\\custom.css"),
+];
